d2b40e283fdf457049234cf2e839c941d5bcf8e8
krishna-bhardwaj012 <109617261+krishna-bhardwaj012@users.noreply.github.com>
2023-02-04 23:14:30 +0530

    Create Final Keyword.java

---
+++ Final Keyword.java
@@ -0,0 +1,15 @@
+class Father{
+    public final String lastName = "Bhardwaj";
+    public final void foodhabit(){
+        System.out.println("I like salty");
+    }
+}
+class son extends Father{
+    public void foodhabit(){  
+    }
+    lastName = "Singh"; 
+}
+
+public class Final_keyword {
+
+}
